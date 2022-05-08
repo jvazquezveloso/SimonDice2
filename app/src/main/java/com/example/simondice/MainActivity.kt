0177c5deac3dbf3c2c60d6d8c8a646ee23b75b7e
Juan <jvazquezveloso@danielcastelao.org>
2022-05-09 01:07:16 +0200

    cambios

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -1,24 +1,72 @@
 package com.example.simondice
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val jugar: Button = findViewById(R.id.jugar)
-        jugar.setOnClickListener(){
-            muestraRonda()
-            ejecutarSecuencia()
+
+        val rojo: Button =findViewById(R.id.rojo)
+        val azul: Button =findViewById(R.id.azul)
+        val verde: Button =findViewById(R.id.verde)
+        val amarillo: Button =findViewById(R.id.amarillo)
+        val jugar: Button =findViewById(R.id.jugar)
+        val cuadroRondas: TextView =findViewById(R.id.textView)
+
+        var rondas = 1
+        var array1 = ArrayList<Int>()
+        var array2 = ArrayList<Int>()
+
+        suspend fun cambioColor(array: ArrayList<Int>){
+            for (color in array){
+                when (color){
+                    1 ->{
+                        rojo.setBackgroundColor(Color.parseColor("#f5a7a2"))
+                        val job = CoroutineScope(Dispatchers.Main).launch{
+                            delay(500)
+                        }
+                        rojo.setBackgroundColor(Color.parseColor("E82929"))
+                        delay(500)
+                    }
+                    2 ->{
+                        azul.setBackgroundColor(Color.parseColor("#a2adfa"))
+                        val job = CoroutineScope(Dispatchers.Main).launch {
+                            delay(500)
+                        }
+                        azul.setBackgroundColor(Color.parseColor("#0F2DD3"))
+                        delay(500)
+                    }
+                    3->{
+                        amarillo.setBackgroundColor(Color.parseColor("#fafaac"))
+                        val job = CoroutineScope(Dispatchers.Main).launch {
+                            delay(500)
+                        }
+                        amarillo.setBackgroundColor(Color.parseColor("#F3DC19"))
+                        delay(500)
+                    }
+                    4->{
+                        verde.setBackgroundColor(Color.parseColor("#bfeda1"))
+                        val job = CoroutineScope(Dispatchers.Main).launch {
+                            delay(500)
+                        }
+                        verde.setBackgroundColor(Color.parseColor("#5FCC1A"))
+                        delay(500)
+                    }
+                }
+            }
         }
-    }
-    private fun muestraRonda(){
 
-    }
-    private fun ejecutarSecuencia(){
+        jugar.setOnClickListener(){
+            cuadroRondas.setText(rondas.toString())
+            array1.clear()
+            array2.clear()
 
-    }
-
+        }
 }
